9ffb3a0c0179edbda3b6e5d6b94ade85263efb4c
kimss373 <kimss373@naver.com>
2022-02-25 20:25:02 +0900

    백준_1057번_토너먼트

--- 백준/실버3/1057번_토너먼트.py
+++ 백준/실버3/1057번_토너먼트.py
@@ -1,23 +1,19 @@
 n, kim, lim = map(int, input().split())
 cnt = 0
-if kim==1 and lim==2:
-    print(1)
-elif lim==1 and kim==2:
-    print(1)
-else:
-    while True:
-        if kim%2==0:
-            kim = kim//2
-        else:
-            kim = kim//2 +1
 
-        if lim%2 == 0:
-            lim = lim//2
-        else:
-            lim = lim//2 +1
+while True:
+    if kim % 2 == 0:
+        kim = kim // 2
+    else:
+        kim = kim // 2 + 1
 
-        cnt += 1
+    if lim % 2 == 0:
+        lim = lim // 2
+    else:
+        lim = lim // 2 + 1
 
-        if kim == lim:
-            print(cnt)
-            break
+    cnt += 1
+
+    if kim == lim:
+        print(cnt)
+        break
