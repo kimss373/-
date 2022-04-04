2028cf5b42da0db4c7fb35f4c553082de7eaa46d
kimss373 <kimss373@naver.com>
2022-04-04 14:29:24 +0900

    프로그래머스_타겟 넘버

---
+++ 프로그래머스/타겟 넘버.py
@@ -0,0 +1,19 @@
+from itertools import product
+plusminus = ['+', '-']
+
+def solution(numbers, target):
+    a = list(product(plusminus, repeat = len(numbers)))
+    answer = 0
+    for i in sorted(a):
+        num = 0
+        for j in range(len(numbers)):
+            if i[j] == '+':
+                num += numbers[j]
+            elif i[j] == '-':
+                num -= numbers[j]
+        if num == target:
+            answer += 1
+    return answer
+
+print(solution([1, 1, 1, 1, 1], 3))
+print(solution([4, 1, 2, 1], 4))
