9755abd99ef7a65b89f9feb56b0899ac1cbbd436
김성수 <kimss373@naver.com>
2022-05-03 18:09:59 +0900

    프로그래머스_베스트앨범

--- 프로그래머스/베스트앨범.py
+++ 프로그래머스/베스트앨범.py
@@ -1,11 +1,28 @@
 def solution(genres, plays):
     answer = []
-    diction = {}
-    for x in zip(genres, plays):
-        if x[0] in diction:
-            diction[x[0]] += x[1]
+    dict = {}
+    record = []
+    for i in range(len(genres)):
+        if genres[i] in dict:
+            dict[genres[i]][1].append([plays[i], i])
+            dict[genres[i]][0]+=plays[i]
         else:
-            diction[x[0]] = x[1]
-    print(diction)
+            dict[genres[i]] = [plays[i], [[plays[i], i]]]
+    for i in dict:
+        record.append([dict[i][0], i])
+        dict[i][1] = sorted(dict[i][1], key=lambda x:-x[0])
+
+    record_sort = sorted(record, key=lambda x:-x[0])
+
+    for i in range(len(record_sort)):
+        if len(dict[record_sort[i][1]][1]) == 1:
+            answer.append(dict[record_sort[i][1]][1][0][1])
+        else:
+            for j in range(2):
+                answer.append(dict[record_sort[i][1]][1][j][1])
+
+    print("dict = ", dict)
+    print("record_sort = ", record_sort)
+
     return answer
 print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))
