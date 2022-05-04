a45393d5661041652554de1eb172491795e95281
김성수 <kimss373@naver.com>
2022-05-04 11:24:14 +0900

    프로그래머스_카펫

--- 프로그래머스/카펫.py
+++ 프로그래머스/카펫.py
@@ -6,7 +6,7 @@ def solution(brown, yellow):
         if yellow % height == 0:
             if brown - 2*((yellow//height) + height) == 4:
                 break
-
+        height += 1
     answer.append((brown+yellow)//(height+2))
     answer.append(height+2)
 
