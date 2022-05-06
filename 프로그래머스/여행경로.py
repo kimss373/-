532aeae176a3055c4857d28bf1df780c507f3c9a
김성수 <kimss373@naver.com>
2022-05-06 16:31:39 +0900

    프로그래머스_여행경로

---
+++ 프로그래머스/여행경로.py
@@ -0,0 +1,23 @@
+def solution(tickets):
+    answer = []
+    dict = {}
+
+    for i in tickets:
+        if i[0] in dict:
+            dict[i[0]].append(i[1])
+        else:
+            dict[i[0]] = [i[1]]
+
+    for i in dict.keys():
+        dict[i].sort(reverse=True)
+
+    start = ["ICN"]
+    while start:
+        target = start[-1]
+        if target not in dict or len(dict[target]) == 0:
+            answer.append(start.pop())
+        else:
+            start.append(dict[target].pop())
+    return list(reversed(answer))
+
+print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
