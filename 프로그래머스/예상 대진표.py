6831489d0fc50e40af0132a65e6d090da02f37b7
kimss373 <kimss373@naver.com>
2022-05-09 18:20:19 +0900

    프로그래머스_예상 대진표

---
+++ 프로그래머스/예상 대진표.py
@@ -0,0 +1,11 @@
+def solution(n, a, b):
+    answer = 1
+
+    while True:
+        if abs(b - a) == 1 and b // 2 != a // 2:
+            break
+        a = a // 2 + a % 2
+        b = b // 2 + b % 2
+
+        answer += 1
+    return answer
