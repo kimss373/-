298d7b556ebb369d833cc3e9a9ddf26249293a68
김성수 <kimss373@naver.com>
2022-05-14 23:16:53 +0900

    프로그래머스_콜라츠 추측

---
+++ 프로그래머스/콜라츠 추측.py
@@ -0,0 +1,19 @@
+def solution(num):
+    answer = 0
+    if num == 1:
+        return answer
+    while True:
+        if num % 2 == 0:
+            num = num // 2
+            answer += 1
+            if num == 1:
+                break
+        else:
+            num = num * 3 + 1
+            answer += 1
+
+        if answer == 500:
+            answer = -1
+            break
+
+    return answer
