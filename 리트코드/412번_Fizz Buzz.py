4e602a0c7b4e45dc1ba74c713eaab6ae869e474c
kimss373 <kimss373@naver.com>
2022-09-16 16:35:34 +0900

    리트코드_Fizz Buzz

---
+++ 리트코드/412번_Fizz Buzz.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        answer = []
+        for i in range(1, n + 1):
+            if i % 3 == 0:
+                if i % 5 == 0:
+                    answer.append("FizzBuzz")
+                    continue
+                answer.append("Fizz")
+
+            elif i % 5 == 0:
+                answer.append("Buzz")
+
+            else:
+                answer.append(str(i))
+
+        return answer
